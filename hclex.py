70db3acaea281852e7bcf1c1573ce4082a331dda
SyfP <>
2021-12-27 00:53:54 +0000

    Add lexer support for init keyword, @, and numbers
    Also allowed the lexer's main method to take an input file path

--- hclex.py
+++ hclex.py
@@ -7,13 +7,16 @@ tokens = (
 	"WS",
 
 	"LET",
+	"INIT",
 	"INPUT",
 	"OUTPUT",
 	"FOREVER",
 
 	"IDENTIFIER",
+	"NUMBER",
 
 	"EQUALS",
+	"AT",
 )
 
 def t_NL(t):
@@ -24,24 +27,49 @@ def t_NL(t):
 t_WS = r"[\t ]+"
 
 t_LET     = r"let"
+t_INIT    = r"init"
 t_INPUT   = r"input"
 t_OUTPUT  = r"output"
 t_FOREVER = r"forever"
 
-t_IDENTIFIER = r"(?!(?:let|forever|input|output)[^a-zA-Z_\d])[a-zA-Z_][a-zA-Z_\d]*"
+t_IDENTIFIER = (r"(?!(?:let|forever|input|output|init)"
+	+ r"[^a-zA-Z_\d])[a-zA-Z_][a-zA-Z_\d]*")
+
+def t_NUMBER(t):
+	r"\d+"
+	t.value = int(t.value)
+	return t
 
 t_EQUALS = r"="
+t_AT     = r"@"
 
 def t_error(t):
 	raise TypeError(f"Unknown text {t.value}")
 
 def main():
-	PATH = "y4-scrambler-handler.hc"
+	import sys
+	import argparse
 
-	for line in open(PATH):
+	parser = argparse.ArgumentParser(description="Lex .hc files")
+	parser.add_argument("input", default=None)
+
+	args = parser.parse_args()
+
+	f = None
+	close_f = False
+	if args.input is None:
+		f = sys.stdin
+	else:
+		f = open(args.input)
+		close_f = True
+
+	for line in f:
 		lex.input(line)
 		for token in iter(lex.token, None):
 			print(token.type, repr(token.value))
+	
+	if close_f:
+		f.close()
 
 lex.lex()
 
