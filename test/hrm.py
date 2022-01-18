20870891482c8788e7895cf7f0052a88b9fcbeda
SyfSchydea <sashapwn@gmail.com>
2022-01-18 20:55:32 +0000

    Add execution to the HRM emulator
    Still only enough to execute the solution to year 1 so far though.

--- test/hrm.py
+++ test/hrm.py
@@ -3,15 +3,74 @@
 class BossError(Exception):
 	pass
 
+class InboxError(Exception):
+	pass
+
+MIN_VALUE = -999
+MAX_VALUE =  999
+
 class Instruction:
 	def __repr__(self):
 		return type(self).__name__ + "()"
 
 class Inbox(Instruction):
-	pass
+	def execute(self, office):
+		office.hands = next(office.inbox)
 
 class Outbox(Instruction):
-	pass
+	def execute(self, office):
+		if office.hands is None:
+			raise BossError("Empty value! You\n"
+					"can't OUTBOX with\n"
+					"empty hands!\n")
+
+		office.outbox.write(str(office.hands) + "\n")
+		office.hands = None
+
+# Holds the runtime for the HRM
+class Office:
+	__slots__ = [
+		# List of Instructions
+		"program",
+
+		# Iterator from which inbox values are drawn
+		"inbox",
+
+		# File to which outbox values are written
+		"outbox",
+
+		# Value held in the worker's hands
+		# Either an int from -999 to 999, or None if empty
+		"hands",
+
+		# Id of next instruction
+		"program_counter",
+	]
+
+	def __init__(self, program):
+		self.program = program
+
+		self.inbox = None
+		self.outbox = None
+
+		self.hands = None
+		self.program_counter = 0
+
+	def execute(self):
+		while self.program_counter < len(self.program):
+			instr = self.program[self.program_counter]
+
+			try:
+				instr.execute(self)
+			except StopIteration:
+				break
+
+			self.program_counter += 1
+
+	def __repr__(self):
+		return ("Office(" + repr(self.program) + ", "
+				+ "hands=" + repr(self.hands) + ", "
+				+ "pc=" + repr(self.program_counter) + ")")
 
 BOSS_PASTE_ERROR = ("You can't paste that\n"
 		"here! It does not appear\n"
@@ -49,7 +108,25 @@ def load_program(path):
 				raise BossError(BOSS_PASTE_ERROR
 						+ f"Unrecognised instruction: '{line}'\n")
 
-	return program
+	return Office(program)
+
+# Generate numbers from the given file
+def read_input(file_in):
+	for line in file_in:
+		line = line.strip()
+
+		if line == "":
+			continue
+
+		try:
+			num = int(line)
+		except ValueError as e:
+			raise InboxError(e)
+
+		if num < MIN_VALUE or num > MAX_VALUE:
+			raise InboxError("number out of bounds: " + repr(num))
+
+		yield num
 
 def main():
 	import sys
@@ -61,12 +138,17 @@ def main():
 	args = parser.parse_args()
 
 	try:
-		program = load_program(args.program)
+		office = load_program(args.program)
 	except BossError as e:
 		sys.stderr.write(str(e))
 		sys.exit(1)
+	except InputError as e:
+		sys.stderr.write(str(e))
+		sys.exit(2)
 
-	print(program)
+	office.inbox = read_input(sys.stdin)
+	office.outbox = sys.stdout
+	office.execute()
 
 if __name__ == "__main__":
 	main()
