d0b4912a0833ddbe720cfb0d75d482cee4c7ca37
SyfSchydea <sashapwn@gmail.com>
2022-01-20 09:55:53 +0000

    Add test for syntax error on empty output statement
    Test currently fails.

---
+++ test/source/errors/empty-output-stmt.hc
@@ -0,0 +1,6 @@
+// This file contains an output statement
+// with no expression attached to it.
+// This should throw an error at the parser level
+// since output statements expect an expression.
+
+output
--- test/test_errors.py
+++ test/test_errors.py
@@ -3,17 +3,31 @@
 import unittest
 import subprocess
 
-class TestLexer(unittest.TestCase):
+class AbstractTests:
+	class TestError(unittest.TestCase):
+		# Check that the given file throws the specified error.
+		# Checks that the expected error is in the stderr output.
+		def assertError(self, src_path, expected_error, msg=None):
+			process = subprocess.run(["./hccompile.py", src_path],
+					capture_output=True)
+
+			self.assertNotEqual(0, process.returncode,
+					"Invalid source should not exit with code 0")
+			self.assertEqual(expected_error,
+					process.stderr.decode().rstrip(), msg)
+
+class TestLexer(AbstractTests.TestError):
 	def test_invalid_char(self):
-		src_path = "test/source/errors/lexer-error.hc"
-
-		process = subprocess.run(["./hccompile.py", src_path],
-				capture_output=True)
-
-		self.assertNotEqual(0, process.returncode,
-				"Invalid source should not exit with code 0")
-		self.assertIn("Unexpected character at line 4, col 3: '~'", process.stderr.decode(),
+		self.assertError("test/source/errors/lexer-error.hc",
+				"Unexpected character at line 4, col 3: '~'",
 				"Lexer errors should produce a useful error message")
+
+class TestParser(AbstractTests.TestError):
+	def test_empty_output_stmt(self):
+		self.assertError("test/source/errors/empty-output-stmt.hc",
+				"Syntax error at '\\n' on line 6, col 7",
+				"Empty output statement should result in "
+						"an informative syntax error")
 
 if __name__ == "__main__":
 	unittest.main()
