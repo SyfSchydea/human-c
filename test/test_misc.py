d4f0ca909382a2c32f2bc3e53d262cd0065cf4d7
SyfSchydea <sashapwn@gmail.com>
2022-01-27 00:29:35 +0000

    Add test for more complex logical and cases

--- test/test_misc.py
+++ test/test_misc.py
@@ -161,3 +161,30 @@ class TestLogicalAnd(AbstractTests.TestValidProgram):
 			([ 10,   5,   8, 13,  10,   9,  3,  10],
 					[10, 5, 8, 13, 10, 9, 3, 10]),
 		])
+
+class TestLogicalAndComplexRight(AbstractTests.TestValidProgram):
+	source_path = "misc/logical-and-complex-right.hc"
+	floor_size = 16
+
+	# This program aims to test for evaluation of the
+	# logical and operator using a more complex right
+	# operand which requires additional statements to
+	# be injected during compilation.
+
+	# This file should read values from the inbox and
+	# output it if and only if it is non-negative and
+	# the following two inputs sum to 0. Those inputs
+	# shouldn't be consumed if the original input was
+	# negative.
+	def test_output(self):
+		self.run_tests([
+			([], []),
+			([13, 12, -12], [13]),
+			([-11], []),
+			([-1, 14, 9, -9], [14]),
+			([10, -9, 3, -10, 21, 12, -12], [21]),
+			([17, -13, 13, 7, 18, -18, 3, 13, -13], [17, 7, 3]),
+			([-13, -4, 7, 5, -5], [7]),
+			([1, 1, -6, 6, -16, 16], [6]),
+			([4, 3, 7, -7, 7, 10, 5, -5, 5, 20, 11, -11], []),
+		])
